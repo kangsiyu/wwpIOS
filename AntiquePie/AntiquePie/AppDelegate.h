1beb5174bdf2f47c9ebbb82f26c0c58d05aa9093
siyukang <ksy@192.168.1.104>
2015-05-13 00:58:34 +0800

    first

---
+++ AntiquePie/AntiquePie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AntiquePie
+//
+//  Created by ksy on 15/5/13.
+//  Copyright (c) 2015年 ksy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
